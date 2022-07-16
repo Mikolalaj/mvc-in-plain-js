34db44330c010ac248908593c205178f0cd22eb1
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2022-07-16 14:44:41 +0200

    finished script

--- script.ts
+++ script.ts
@@ -1,19 +1,19 @@
-type Rating = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | null // "dirty" solution but it's best here
-
 type Movie = {
     id: number
     title: string
     watched: boolean
-    rating: Rating
 }
 
 class Model {
     movies: Movie[]
+    onMovieListChanged: (movies: Movie[]) => void
     constructor() {
-        this.movies = [
-            { id: 1, title: 'Run a marathon', watched: false, rating: null },
-            { id: 2, title: 'Plant a garden', watched: true, rating: 7 },
-        ]
+        this.movies = JSON.parse(localStorage.getItem('movies')) || []
+    }
+
+    _commit(movies: Movie[]) {
+        this.onMovieListChanged(movies)
+        localStorage.setItem('movies', JSON.stringify(movies))
     }
   
     addMovie(title: string) {
@@ -25,22 +25,34 @@ class Model {
         }
   
         this.movies.push(newMovie)
+
+        this._commit(this.movies)
     }
   
-    editMovie(id: number, title: string, rating: Rating) {
+    editMovie(id: number, title: string) {
         this.movies = this.movies.map(movie =>
-            movie.id === id ? { ...movie, title: title, rating: rating } : movie,
+            movie.id === id ? { ...movie, title: title } : movie,
         )
+
+        this._commit(this.movies)
     }
 
     deleteMovie(id: number) {
         this.movies = this.movies.filter(movie => movie.id !== id)
+
+        this._commit(this.movies)
     }
 
     toggleMovie(id: number) {
         this.movies = this.movies.map(movie =>
-            movie.id === id ? { ...movie, watched: !movie.watched, watchDate: movie.watched ? null : new Date() } : movie,
+            movie.id === id ? { ...movie, watched: !movie.watched } : movie,
         )
+
+        this._commit(this.movies)
+    }
+
+    bindMovieListChanged(callback: (movies: Movie[]) => void) {
+        this.onMovieListChanged = callback
     }
 }
   
@@ -51,6 +63,7 @@ class View {
     input: HTMLInputElement
     submitButton: HTMLButtonElement
     moviesList: HTMLUListElement
+    private _temporaryMovieTitle: string
 
     constructor() {
         this.app = this.getElement('#root')
@@ -60,7 +73,7 @@ class View {
 
         this.form = this.createElement('form') as HTMLFormElement
 
-        this.input = this.createElement('input') as HTMLInputElement
+        this.input = this.createElement('input', 'movie-title') as HTMLInputElement
         this.input.type = 'text'
         this.input.placeholder = 'Movie title'
         this.input.name = 'movie'
@@ -73,6 +86,29 @@ class View {
         this.form.append(this.input, this.submitButton)
 
         this.app.append(this.title, this.form, this.moviesList)
+
+        this._initLocalListeners()
+    }
+
+    // Update temporary state
+    private _initLocalListeners() {
+        this.moviesList.addEventListener('input', (event: any) => {
+            if (event.target.className === 'editable') {
+                this._temporaryMovieTitle = event.target.innerText
+            }
+        })
+    }
+
+    // Send the completed value to the model
+    bindEditMovie(handler: (id: number, title: string) => void) {
+        this.moviesList.addEventListener('focusout', (event: any) => {
+        if (this._temporaryMovieTitle) {
+            const id = parseInt(event.target.parentElement.id)
+    
+            handler(id, this._temporaryMovieTitle)
+            this._temporaryMovieTitle = ''
+        }
+        })
     }
 
     displayMovies(movies: Movie[]) {
@@ -89,30 +125,31 @@ class View {
             movies.forEach(movie => {
                 const li = this.createElement('li') as HTMLLIElement
                 li.id = movie.id.toString()
+
+                // Each movie item will have a checkbox you can toggle
+                const checkbox = this.createElement('input') as HTMLInputElement
+                checkbox.type = 'checkbox'
+                checkbox.checked = movie.watched
             
                 // The movie item text will be in a contenteditable span
                 const span = this.createElement('span') as HTMLSpanElement
                 span.contentEditable = 'true'
                 span.classList.add('editable')
             
-                const ratingWrapper = this.createElement('span') as HTMLSpanElement
-
-                const rating = this.createElement('span') as HTMLSpanElement
-                rating.contentEditable = 'true'
-                rating.classList.add('editable')
-                rating.textContent = movie.rating !== null ? movie.rating.toString() : ''
-
-                const ratingRange = this.createElement('span') as HTMLSpanElement
-                ratingRange.textContent = '/10'
-
-                ratingWrapper.append(rating, ratingRange)
-
-                span.textContent = movie.title
+                // If the movie is watched, it will have a strikethrough
+                if (movie.watched) {
+                    const strike = this.createElement('s') as HTMLTimeElement
+                    strike.textContent = movie.title
+                    span.append(strike)
+                } else {
+                    // Otherwise just display the title
+                    span.textContent = movie.title
+                }
 
                 // The movies will also have a delete button
                 const deleteButton = this.createElement('button', 'delete') as HTMLButtonElement
                 deleteButton.textContent = 'Delete'
-                li.append(ratingWrapper, span, deleteButton)
+                li.append(checkbox, span, deleteButton)
             
                 // Append nodes to the movies list
                 this.moviesList.append(li)
@@ -140,6 +177,37 @@ class View {
     _resetInput() {
         this.input.value = ''
     }
+
+    bindAddMovie(handler: (title: string) => void) {
+        this.form.addEventListener('submit', event => {
+            event.preventDefault()
+      
+            if (this._movieText) {
+                handler(this._movieText)
+                this._resetInput()
+            }
+        })
+    }
+      
+    bindDeleteMovie(handler: (id: number) => void) {
+        this.moviesList.addEventListener('click', (event: any) => {
+            if (event.target.className === 'delete') {
+                const id = parseInt(event.target.parentElement.id)
+                
+                handler(id)
+            }
+        })
+    }
+      
+    bindToggleMovie(handler: (id: number) => void) {
+        this.moviesList.addEventListener('change', (event: any) => {
+            if (event.target.type === 'checkbox') {
+                const id = parseInt(event.target.parentElement.id)
+        
+                handler(id)
+            }
+        })
+    }
 }
   
 class Controller {
@@ -150,25 +218,31 @@ class Controller {
         this.view = view
         // Display initial movies
         this.onMovieListChanged(this.model.movies)
+
+        this.view.bindAddMovie(this.handleAddMovie)
+        this.view.bindDeleteMovie(this.handleDeleteMovie)
+        this.view.bindToggleMovie(this.handleToggleMovie)
+        this.view.bindEditMovie(this.handleEditMovie)
+        this.model.bindMovieListChanged(this.onMovieListChanged)
     }
 
-    onMovieListChanged(movies: Movie[]) {
+    onMovieListChanged = (movies: Movie[]) => {
         this.view.displayMovies(movies)
     }
 
-    handleAddMovie(movieTitle: string) {
+    handleAddMovie = (movieTitle: string) => {
         this.model.addMovie(movieTitle)
     }
       
-    handleEditMovie(id: number, movieTitle: string, rating: Rating) {
-        this.model.editMovie(id, movieTitle, rating)
+    handleEditMovie = (id: number, movieTitle: string) => {
+        this.model.editMovie(id, movieTitle)
     }
       
-    handleDeleteMovie(id: number) {
+    handleDeleteMovie = (id: number) => {
         this.model.deleteMovie(id)
     }
       
-    handleToggleMovie(id: number) {
+    handleToggleMovie = (id: number) => {
         this.model.toggleMovie(id)
     }
 }
